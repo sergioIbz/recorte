d985510a2037ddc1982b1cb2f3b70b80fc7a040a
sergioIbgz <sergioibagnez@gmail.com>
2024-03-13 00:36:43 -0500

    animacion

--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,90 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'CustomClipper',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('CustomClipper'),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FilledButton(
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'CustomClipper',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const TestCustomClipler(),
+            ),
+          ),
         ),
-        body: Center(
-          child: ClipPath(
-            clipper: PruebaClipper(),
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  width: 300, // Ancho del contenedor
-                  height: 400, // Altura del contenedor
-                  decoration: const BoxDecoration(
-                    color: Colors.amberAccent,
-                    shape: BoxShape.circle,
-                  ),
+      ),
+    );
+  }
+}
+
+class TestCustomClipler extends StatelessWidget {
+  const TestCustomClipler({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CustomClipper'),
+      ),
+      body: Center(
+        child: ClipPath(
+          clipper: PruebaClipper(),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 2),
+                width: 300, // Ancho del contenedor
+                height: 400, // Altura del contenedor
+                decoration: const BoxDecoration(
+                  color: Colors.amberAccent,
+                  shape: BoxShape.circle,
                 ),
-                Positioned(
-                  bottom: -60,
+              ),
+              Positioned(
+                bottom: -50,
+                child: TweenAnimationBuilder<double>(
+                  curve: Curves.bounceOut,
+                  duration: const Duration(
+                    seconds: 3,
+                  ),
+                  tween: Tween(begin: 1.0, end: 0.0),
                   child: Image.network(
                     'https://www.calfruitos.com/fotos/pr_8957_20230724145623.png',
                     fit: BoxFit.cover,
                   ),
+                  builder: (BuildContext context, double value, Widget? child) {
+                    return Transform.translate(
+                      offset: Offset(0, 300 * value),
+                      child: child!,
+                    );
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
@@ -86,7 +140,6 @@ class MyPaint extends CustomPainter {
 class PruebaClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-   
     final halfWidth = size.width / 2;
     Path path = Path()
       ..lineTo(0, 0)
@@ -95,9 +148,9 @@ class PruebaClipper extends CustomClipper<Path> {
           center: Offset(halfWidth, size.height / 2),
           radius: halfWidth,
         ),
-        pi, 
-        -pi, 
-        false, 
+        pi,
+        -pi,
+        false,
       )
       ..lineTo(size.width, 0.0);
 
